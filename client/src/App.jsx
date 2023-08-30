3bc3b4a7f89732fc1eafb49647ace2a5dbff8296
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-30 14:24:58 +0300

    Assemble components in App.js and set up entry point in main.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
-import './App.css';
+// App.js
+import React from 'react';
+import Header from './components/Header';
+import SongList from './components/SongList';
+import Player from './components/Player';
 
 function App() {
   return (
-    <div>
-      
+    <div className="bg-gray-100 min-h-screen">
+      <Header />
+      <div className="container mx-auto p-4">
+        <SongList />
+        <Player />
+      </div>
     </div>
   );
 }
