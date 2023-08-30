1d9acb65b1a5aece05f5731a545834f18694f12d
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-30 14:15:26 +0300

    Create Header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+// components/Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <h1 className="text-2xl font-bold">TuneHub Music Player</h1>
+    </header>
+  );
+}
+
+export default Header;
