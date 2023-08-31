9cf2bb78198e016a195060e97af3ac3067f2c810
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-31 18:21:27 +0300

    Add songs

--- client/src/components/SongList.jsx
+++ client/src/components/SongList.jsx
@@ -7,20 +7,55 @@ const sampleSongs = [
     id: 1,
     title: 'Song Title 1',
     artist: 'Artist 1',
-    albumArt: 'url_to_album_art_1',
-    audioUrl: 'url_to_audio_1',
+    albumArt: 'https://myretrobucket.s3.eu-north-1.amazonaws.com/albumart1.jpeg',
+    audioUrl: 'https://soundcloud.com/afrobeatgod/timaya-ft-buju-cold-outside?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
   },
-  // Add more songs here
+
+  {
+    id: 2,
+    title: 'Song Title 1',
+    artist: 'Artist 1',
+    albumArt: 'https://myretrobucket.s3.eu-north-1.amazonaws.com/albumart1.jpeg',
+    audioUrl: 'https://soundcloud.com/afrobeatgod/timaya-ft-buju-cold-outside?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
+  },
+  {
+    id: 3,
+    title: 'Song Title 1',
+    artist: 'Artist 1',
+    albumArt: 'https://myretrobucket.s3.eu-north-1.amazonaws.com/albumart1.jpeg',
+    audioUrl: 'https://soundcloud.com/afrobeatgod/timaya-ft-buju-cold-outside?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
+  },
+  {
+    id: 4,
+    title: 'Song Title 1',
+    artist: 'Artist 1',
+    albumArt: 'https://myretrobucket.s3.eu-north-1.amazonaws.com/albumart1.jpeg',
+    audioUrl: 'https://soundcloud.com/afrobeatgod/timaya-ft-buju-cold-outside?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
+  },
+  {
+    id: 5,
+    title: 'Song Title 1',
+    artist: 'Artist 1',
+    albumArt: 'https://myretrobucket.s3.eu-north-1.amazonaws.com/albumart1.jpeg',
+    audioUrl: 'https://soundcloud.com/afrobeatgod/timaya-ft-buju-cold-outside?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
+  },
+  {
+    id: 6,
+    title: 'Song Title 1',
+    artist: 'Artist 1',
+    albumArt: 'https://myretrobucket.s3.eu-north-1.amazonaws.com/albumart1.jpeg',
+    audioUrl: 'https://soundcloud.com/afrobeatgod/timaya-ft-buju-cold-outside?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
+  },
 ];
 
-function SongList() {
-  return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {sampleSongs.map((song) => (
-        <SongCard key={song.id} song={song} />
-      ))}
-    </div>
-  );
-}
-
-export default SongList;
+function SongList({ onSongSelect }) {
+    return (
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {sampleSongs.map((song) => (
+          <SongCard key={song.id} song={song} onSongSelect={onSongSelect} />
+        ))}
+      </div>
+    );
+  }
+  
+  export default SongList;
