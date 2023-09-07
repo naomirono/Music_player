f600e5d560c8ff2009b49d24171a1f58a1892fea
Naomi Cherotich <naomirono1454@gmail.com>
2023-09-07 21:59:51 +0300

    Update app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-// App.js
-import React from 'react';
+// components/App.js
+import React, { useState } from 'react';
 import Header from './components/Header';
 import SongList from './components/SongList';
 import Player from './components/Player';
 
 function App() {
+  const [selectedSong, setSelectedSong] = useState(null); // or initialize with a default song
+
   return (
     <div className="bg-gray-100 min-h-screen">
       <Header />
       <div className="container mx-auto p-4">
-        <SongList />
-        <Player />
+        <SongList onSongSelect={setSelectedSong} />
+        <Player selectedSong={selectedSong} />
       </div>
     </div>
   );
