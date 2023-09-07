8a2f6c36d72a03ccf8167bbc6a4fac9442fc3f19
Naomi Cherotich <naomirono1454@gmail.com>
2023-09-07 23:04:12 +0300

    Add sign in and login buttons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <nav className="bg-gray-800 p-4 text-white">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
-          <div className="text-2xl font-bold ">Music Player</div>
+          <div className="text-2xl font-bold">Music Player</div>
           <div className="space-x-4">
             <a href="#" className="text-white hover:text-turquoise-500">Home</a>
             <a href="#" className="text-white hover:text-turquoise-500">Library</a>
@@ -21,6 +21,8 @@ const Header = () => {
             <div className="text-turquoise-500">
               <i className="fas fa-user-circle text-xl"></i>
             </div>
+            <button className="bg-turquoise-500 border border-[#4DB6AC] text-white px-3 py-1 rounded">Sign Up</button>
+            <button className="bg-turquoise-500 border border-[#4DB6AC] text-white px-3 py-1 rounded">Login</button>
           </div>
         </div>
       </div>
