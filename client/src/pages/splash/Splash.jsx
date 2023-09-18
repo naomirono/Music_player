9d0f6c1191b0ca5359951cab5543a4d17253fc5a
Naomi Cherotich <naomirono1454@gmail.com>
2023-09-18 19:57:43 +0300

    Add image width

--- client/src/pages/splash/Splash.jsx
+++ client/src/pages/splash/Splash.jsx
@@ -4,7 +4,7 @@ import logo from '../../assets/musicicon.png';
 const Splash = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-[#3f51b5]">
-    <img src={logo} alt="Logo" className="mb-4" style={{ width: '200px' }} />
+    <img src={logo} alt="Logo" className="mb-4" style={{ width: '210px' }} />
     <h1 className="text-5xl font-bold text-white mb-8">TuneHub</h1>
   </div>
   );
