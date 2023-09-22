163f5121209ebc5ba21e665b63edbc5bf57aeac8
Naomi Cherotich <naomirono1454@gmail.com>
2023-09-22 18:16:22 +0300

    Delete imports and commented code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
 // components/App.js
 import React, { useState } from 'react';
-import Header from './components/Header';
-import SongList from './components/SongList';
-import Player from './components/Player';
+import TopBar from './components/TopBar';
+import Splash from './pages/splash/Splash';
+import Sidebar from './components/Sidebar';
+import Hero from './components/Hero';
+
 
 function App() {
   const [selectedSong, setSelectedSong] = useState(null); // or initialize with a default song
 
   return (
     <div className="bg-gray-100 min-h-screen">
-      <Header />
-      <div className="container mx-auto p-4">
-        <SongList onSongSelect={setSelectedSong} />
-        <Player selectedSong={selectedSong} />
-      </div>
+      {/* <Splash/> */}
+      <Sidebar/>
+      <TopBar/>
+      <Hero/>
     </div>
   );
 }
