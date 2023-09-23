2251fc237027b3adf5fe1a6734abe121709dfb20
Naomi Cherotich <naomirono1454@gmail.com>
2023-09-23 16:11:08 +0300

    Add animation to logo icon

--- client/src/pages/splash/Splash.jsx
+++ client/src/pages/splash/Splash.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from '../../assets/musicicon.png'; 
+import '../../splash.css'; // Import your CSS file
 
 const Splash = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-[#3f51b5]">
-    <img src={logo} alt="Logo" className="mb-4" style={{ width: '210px' }} />
-    <h1 className="text-5xl font-bold text-white mb-8">TuneHub</h1>
-  </div>
+      <img src={logo} alt="Logo" className="logo-animation mb-4" style={{ width: '210px' }} />
+      <h1 className="text-5xl font-bold text-white mb-8">TuneHub</h1>
+    </div>
   );
 };
 
