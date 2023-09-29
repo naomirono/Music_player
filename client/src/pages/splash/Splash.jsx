7e54024007331271c4bcae458d003893fa0889db
Naomi Cherotich <naomirono1454@gmail.com>
2023-09-29 22:09:32 +0300

    Add a span to the h1 title

--- client/src/pages/splash/Splash.jsx
+++ client/src/pages/splash/Splash.jsx
@@ -6,7 +6,7 @@ const Splash = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-[#3f51b5]">
       <img src={logo} alt="Logo" className="logo-animation mb-4" style={{ width: '210px' }} />
-      <h1 className="text-5xl font-bold text-white mb-8">TuneHub</h1>
+      <h1 className="text-5xl font-bold text-white mb-8">Tune<span className='text-[#1699c8]'>Hub</span></h1>
     </div>
   );
 };
